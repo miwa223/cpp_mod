c0d7e0152e34547b05882500530fe930ec8590c0
増渕美和 <j.aimeparis93@gmail.com>
2022-05-17 17:14:46 +0900

    modify ex00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,28 +1,14 @@
 #include <iostream>
 #include <string>
 
-struct megaphone {
-	const char	*str;
-
-	megaphone(const char *s) {
-		str = s;
-	}
-
-	void	printn() {
-		printf("%s\n", str);
-	}
-};
-
 int	main(int argc, char *argv[]) {
-	std::string	s = ""; //クラス使わずにdel呼べる？無理だったらクラス使うバージョンに変更する
+	std::string	s;
 	int			i = 1;
-	megaphone	s = { argv };
-	s.printn();
 
 	if (argc == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return 0;
+		return (0);
 	}
 	while (argv[i])
 	{
@@ -36,5 +22,5 @@ int	main(int argc, char *argv[]) {
 		i++;
 	}
 	std::cout << s << std::endl;
-	return 0;
+	return (0);
 }
