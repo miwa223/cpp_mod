f8b49d8abc0a875eddcf6c4085ce7f3361469bfc
増渕美和 <j.aimeparis93@gmail.com>
2022-06-25 17:10:54 +0900

    commit ex00

---
+++ 02/ex00/Fixed.cpp
@@ -0,0 +1,36 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	this->fp_num = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = fixed;
+}
+
+Fixed	&Fixed::operator=(const Fixed &fixed)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->fp_num = fixed.getRawBits();
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fp_num);
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	this->fp_num = raw;
+}
