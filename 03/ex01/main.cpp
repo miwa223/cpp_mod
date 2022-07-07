6797be71584d9afc298f392700142da132c155cf
増渕美和 <j.aimeparis93@gmail.com>
2022-07-07 23:21:43 +0900

    modify a bit

--- 03/ex01/main.cpp
+++ 03/ex01/main.cpp
@@ -4,12 +4,9 @@ int	main(void)
 {
 	ScavTrap	A("A");
 	ScavTrap	B("B");
-	ScavTrap	C;
-	C = A;
 
 	A.guardGate();
 	A.attack(B.getName());
 	B.takeDamage(A.getAttackDamage());
-	C.beRepaired(5);
 	return (0);
 }
