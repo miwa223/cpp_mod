270abb32923870aa25c0c866065a5598104605be
miwa <61976102+miwa223@users.noreply.github.com>
2022-07-16 13:52:19 +0900

    add tests for intern class

--- 05/ex03/main.cpp
+++ 05/ex03/main.cpp
@@ -2,18 +2,19 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 
 void	test_signExecShrubberyForm()
 {
 	std::cout << "\n* test_signExecShrubberyForm *" << std::endl;
 	try
 	{
-		int	grade;
-		ShrubberyCreationForm shrubbery("home");
+		int						grade;
+		ShrubberyCreationForm	shrubbery("home");
 		std::cout << shrubbery;
 		std::cout << ">> Type the grade: ";
 		std::cin >> grade;
-		Bureaucrat	A("A", grade);
+		Bureaucrat				A("A", grade);
 
 		std::cout << A;
 		shrubbery.beSigned(A);
@@ -31,12 +32,12 @@ void	test_signExecRobotomyForm()
 	std::cout << "\n* test_signExecRobotomyForm *" << std::endl;
 	try
 	{
-		int	grade;
+		int					grade;
 		RobotomyRequestForm	robot("housing construction");
 		std::cout << robot;
 		std::cout << ">> Type the grade: ";
 		std::cin >> grade;
-		Bureaucrat	B("B", grade);
+		Bureaucrat			B("B", grade);
 
 		std::cout << B;
 		robot.beSigned(B);
@@ -54,12 +55,12 @@ void	test_signExecPresidentialForm()
 	std::cout << "\n* test_signExecPresidentialForm *" << std::endl;
 	try
 	{
-		int	grade;
-		PresidentialPardonForm	president;
+		int						grade;
+		PresidentialPardonForm	president("PersonX");
 		std::cout << president;
 		std::cout << ">> Type the grade: ";
 		std::cin >> grade;
-		Bureaucrat	C("C", grade);
+		Bureaucrat				C("C", grade);
 
 		std::cout << C;
 		president.beSigned(C);
@@ -85,12 +86,48 @@ void	test_ng_inConstructor()
 	}
 }
 
+void	test_ok_intern()
+{
+
+	std::cout << "\n* test_ok_intern *" << std::endl;
+	Bureaucrat				bureaucrat("D", 1);
+	Intern					intern;
+	Form					*ppf;
+	std::string				target = "PersonX";
+	// ShrubberyCreationForm	shrubbery(target);
+	// RobotomyRequestForm		robot(target);
+	PresidentialPardonForm	president(target);
+
+	ppf = intern.makeForm(president.getName(), target);
+	std::cout << *ppf;
+	try
+	{
+		ppf->beSigned(bureaucrat);
+		ppf->execute(bureaucrat);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	delete ppf;
+}
+
+void	test_ng_intern()
+{
+	std::cout << "\n* test_ng_intern *" << std::endl;
+	Intern		intern;
+
+	intern.makeForm("doesn't_exist", "PersonX");
+}
+
 int	main()
 {
-	test_signExecShrubberyForm();
-	test_signExecRobotomyForm();
-	test_signExecPresidentialForm();
+	// test_signExecShrubberyForm();
+	// test_signExecRobotomyForm();
+	// test_signExecPresidentialForm();
 	// test_ng_inConstructor();
+	test_ok_intern();
+	test_ng_intern();
 	// system("leaks -q bureaucrat");
 	return (0);
 }
